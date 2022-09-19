543633bfc34398f0395a4b8e662ce37013f5483a
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 17:36:26 -0500

    task 6, print all possible different combination of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def run():
+    for numbers1 in range(0, 9):
+        for numbers2 in range(1, 10):
+            if numbers1 < numbers2:
+                print("{}{}, ".format(numbers1, numbers2), end="")
+                if numbers1 == 8 and numbers2 == 9:
+                    print("{}{}".format(numbers1, numbers2))
+
+if __name__ == '__main__':
+    run()
