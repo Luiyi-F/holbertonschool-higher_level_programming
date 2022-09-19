96ecf46ad7443c68211b7138f8f7442138cb7152
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 17:08:19 -0500

    task 5, print numbers form 0 to 99

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for numbers in range(00, 100):
+    if numbers != 99:
+        print("{:02}, ".format(numbers), end="")
+    if numbers == 99:
+        print("{:02}".format(numbers))
