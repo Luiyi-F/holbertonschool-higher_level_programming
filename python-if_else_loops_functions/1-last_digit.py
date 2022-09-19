3730b1346f36e0f74c3c76656422e6a43b8f760f
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 16:07:57 -0500

    task 1, use if statement for comprobate the las digit of the number

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+def run():
+    if number > 0:
+        last_digit = number % 10
+    if number < 0:
+        last_digit = (abs(number) % 10) *-1
+
+    print(f"Last digit of {number} is ", end="")
+
+    if last_digit > 5:
+        print(f"{last_digit} and is greater than 5")
+
+    if last_digit < 6 and last_digit != 0:
+        print(f"{last_digit} and is less than 6 and not 0")
+
+    if last_digit == 0:
+        print(f"{last_digit} and is 0")    
+
+if __name__ == '__main__':
+    run()
