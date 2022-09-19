9d194411f549b8043d0c004cc242153a975d36aa
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 17:39:28 -0500

    task 6, print two time 89 FIXED

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,10 @@
 def run():
     for numbers1 in range(0, 9):
         for numbers2 in range(1, 10):
-            if numbers1 < numbers2:
-                print("{}{}, ".format(numbers1, numbers2), end="")
+            if numbers1 < numbers2 :
                 if numbers1 == 8 and numbers2 == 9:
                     print("{}{}".format(numbers1, numbers2))
+                print("{}{}, ".format(numbers1, numbers2), end="")
 
 if __name__ == '__main__':
     run()
