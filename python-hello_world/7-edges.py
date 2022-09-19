492982ba5f818b78a764b885630b8b39e7378606
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 13:21:41 -0500

    task 7, organized comments 2.0

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = f"{word[:3]}"    # take only 3 first letters
 word_last_2 = f"{word[-2:]}"    # take only 2 last letters
-middle_word = f"{word[1:-1]}"   # take only middle letters
+middle_word = f"{word[1:-1]}"   # no take first and last letter
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
