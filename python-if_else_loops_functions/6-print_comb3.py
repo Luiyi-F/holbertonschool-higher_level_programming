015599e979e9b4331cf59b1d07909a01087e53cf
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 17:53:35 -0500

    task 6, not print 9 and 8 FIXED

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ def run():
     for numbers1 in range(0, 9):
         for numbers2 in range(1, 10):
             if numbers2 > numbers1:
-                if numbers1 != 8 and numbers2 != 9:
+                if not (numbers1 == 8 and numbers2 == 9):
                     print("{}{}, ".format(numbers1, numbers2), end="")
                 if numbers1 == 8 and numbers2 == 9:
                     print("{}{}".format(numbers1, numbers2))
