0b4cc031f0f6ff8a61aa6e33b4e32e2e4f9fedfc
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-19 13:16:20 -0500

    task 7, delete comments

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = f"{word[:3]}" # use slides but take only 3 first letters
-word_last_2 = f"{word[-2:]}" # use slides but take only 2 last letters
-middle_word = f"{word[1:-1]}"# use slides but no take the firts adn last letters
-print(f"First 3 letters: {word_first_3}") # output: hol
-print(f"Last 2 letters: {word_last_2}") # output: on
-print(f"Middle word: {middle_word}") # output: olberto
+word_first_3 = f"{word[:3]}"
+word_last_2 = f"{word[-2:]}"
+middle_word = f"{word[1:-1]}"
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
