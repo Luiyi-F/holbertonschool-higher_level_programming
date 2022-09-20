1ac4cf9245200d3ff45cfe14c441d7533ebe283b
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-20 17:09:38 -0500

    [TASK 4, FEATURE]: prints all the names defined by the compiled module

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+def run():
+    for indx in dir(hidden_4):
+        if indx[:2] != "__":
+            print("{}".format(indx))
+
+if __name__ == "__main__":
+    run()
