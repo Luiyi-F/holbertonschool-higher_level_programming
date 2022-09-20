e6e1d9920b9528cad959eafe881e13f9e64b19c3
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-20 14:47:20 -0500

    [TASK 12, FEATURES]: prints the numbers from 1 to 100 separated by a space For multiples of three print Fizz instead of the number and for multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for numbers in range(1, 101):
+        if numbers % 3 == 0:
+            print("Fizz", end=" ")
+        if numbers % 5 == 0:
+            print("Buzz", end=" ")
+        if numbers % 3 == 0 and numbers % 5 == 0:
+            print("FizzBuzz", end=" ")
+        if numbers % 3 != 0 and numbers % 5 != 0:
+            print("{}".format(numbers), end=" ")
