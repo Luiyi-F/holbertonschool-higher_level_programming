eb91b6e01ce4e0ee18f56ae3b3d3645c5f177d0a
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-20 13:54:27 -0500

    task 8, [FIXED]: used the mandatory string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        """print uppercase string"""
         if ord(char) >= 97 and ord(char) <= 122:
-            char = chr(ord(char) - 32)
-        print("{}".format(char), end="")
+            char = ord(char) - 32
+        else:
+            char = ord(char)
+        print("{}".format(chr(char)), end="")
     print("")
