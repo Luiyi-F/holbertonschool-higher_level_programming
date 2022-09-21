5c98bc6abe1cae90b57995d9416a5cf7d24cd30c
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-21 10:35:45 -0500

    [TASK 1, FEATURES]: retrieves an element from a list like in C

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+    elif idx > len(my_list):
+        return (None)
+    for number in range(0, len(my_list)):
+        if number == idx:
+            return (my_list[number])
