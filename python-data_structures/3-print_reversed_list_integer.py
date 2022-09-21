a8fba0de4b2e7a312deaf3a8e668927892a87728
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-21 18:56:40 -0500

    [ALL TASK, FIXED]: delete first white space

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_reversed_list_integer(my_list=[]):
     if isinstance(my_list, list):
         for indx in my_list[::-1]:
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
     for indx in matrix:
         for value in range(len(indx)):
