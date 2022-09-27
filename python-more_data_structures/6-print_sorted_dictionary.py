245ddaa40867c7ae48843bc81718cd20decf8d60
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-27 14:40:43 -0500

    [TASK 6, FIXED]: add dobble point :

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = {key: value for key, value in sorted(a_dictionary.items())}
     for dict_key, dict_value in sorted_dict.items():
-        print("{} {}". format(dict_key, dict_value))
+        print("{}: {}". format(dict_key, dict_value))
