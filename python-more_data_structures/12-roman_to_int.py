c26e1362c04ea77f9a254c7f0b49a789fd4fa844
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-27 18:48:16 -0500

    [TASK 12, FIXED]: only resive str

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or len(roman_string) is 0:
+        return (0)
     roman_num = {'I': 1, 'V': 5, 'X': 10,
                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
