b0cc8d59a02c8cf5e9531e2d9688d4ee3ebbf2d7
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-27 14:39:32 -0500

    [TASK 6, FIXED]: delete import modules

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from curses import REPORT_MOUSE_POSITION
-
-
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = {key: value for key, value in sorted(a_dictionary.items())}
     for dict_key, dict_value in sorted_dict.items():
