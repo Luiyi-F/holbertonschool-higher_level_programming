519eeaa37221cf4c450e74ca9f388876fddcc407
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-27 19:29:03 -0500

    [TASK 9, FIXED]: PEP8

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     square_dict = {}
-    
     for key in a_dictionary:
         square_dict[key] = a_dictionary[key] * 2
     return (square_dict)
