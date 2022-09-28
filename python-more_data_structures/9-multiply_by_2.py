49f8a4cd0cedc6f3e6c2d2725ac36d1bd248b859
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-27 19:28:29 -0500

    [TASK 9, FIXED]: correct ouput when the input is XX

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     square_dict = {}
-    if (a_dictionary):
-        for key, value in a_dictionary.items():
-            square_dict.update({key: value * 2})
-        return (square_dict)
+    
+    for key in a_dictionary:
+        square_dict[key] = a_dictionary[key] * 2
+    return (square_dict)
