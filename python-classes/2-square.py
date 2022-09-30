9b32c19ed0219c8e0bebc4c515a58cb43d69b844
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-30 14:53:48 -0500

    [TASK 2, FIXED]: PEP8

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,8 +14,8 @@ class Square:
     """initalization class Squiare"""
 
     def __init__(self, size=0):
-        """Valitation if the size is to integer 
-        or is a negative number"""
+        """Valitation if the size is to
+         integer or is a negative number"""
         if not isinstance(size, int):
             raise (TypeError("size must be an integer"))
         if int(size) < 0:
