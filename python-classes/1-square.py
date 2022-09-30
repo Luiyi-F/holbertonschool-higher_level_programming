ea15eabacf0ee0a26ab08509c7a093cf5c179c17
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-30 09:30:53 -0500

    [TASK 1, FIXED]: add complete comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
-Create square class and instance attribute
+Write a class Square that defines a square by: (based on 0-square.py)
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
 """
 
 
