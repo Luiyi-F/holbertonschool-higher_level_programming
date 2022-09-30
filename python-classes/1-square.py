8314cf331ca6ae76f58a228f11f4bbef9e370ada
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-30 09:19:07 -0500

    [TASK 1, FIXED]: add comments in the module, class and instance

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """
-Class Square
+Create square class and instance attribute
 """
+
+
 class Square:
+    """initalization class Squiare"""
+
     def __init__(self, size):
         """Initialization self.size"""
         self.__size = size
