5e572b6d010987ed2ef76212ebc6dac66942f4ec
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-30 21:50:56 -0500

    [TASK 6, FIXED]: PEP8 and add comments

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,8 +8,9 @@ class Square:
     """initalization class Squiare"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Method: Square object
-        args: size of the square"""
+        """initialization method of attribute instance
+            use size and position
+            """
 
         if not isinstance(size, int):
             raise (TypeError("size must be an integer"))
@@ -21,35 +22,37 @@ class Square:
                 or not all(number >= 0 for number in position):
             raise (TypeError("position must be a tuple \
 of 2 positive integers"))
-
+        # private instance attributes
         self.__position = position
         self.__size = size
 
     @property
     def size(self):
+        """getter size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """setter size"""
         if not isinstance(value, int):
             raise (TypeError("size must be an integer"))
         if int(value) < 0:
             raise (ValueError("size must be >= 0"))
-
         self.__size = value
 
     @property
     def position(self):
+        """getter position"""
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """setter position"""
         if not isinstance(value, tuple) or len(value) != 2 \
                 or not all(isinstance(number, int) for number in value)\
                 or not all(number >= 0 for number in value):
             raise (TypeError("position must be a tuple \
 of 2 positive integers"))
-
         self.__position = value
 
     def area(self):
@@ -58,6 +61,7 @@ of 2 positive integers"))
         return (area_sqr)
 
     def my_print(self):
+        """print square with the size"""
         if self.__size == 0:
             print()
             return
