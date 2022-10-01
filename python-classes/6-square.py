6b2b81c88d25e5de3d40c87f52036f628eabd894
Luiyi_FHB <luiyifernando5@gmail.com>
2022-09-30 21:28:45 -0500

    [TASK 6, FIXED]: PEP8

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -19,7 +19,8 @@ class Square:
         if not isinstance(position, tuple) or len(position) != 2 \
                 or not all(isinstance(number, int) for number in position)\
                 or not all(number >= 0 for number in position):
-            raise (TypeError("position must be a tuple of 2 positive integers"))
+            raise (TypeError("position must be a tuple \
+            of 2 positive integers"))
 
         self.__position = position
         self.__size = size
@@ -46,7 +47,8 @@ class Square:
         if not isinstance(value, tuple) or len(value) != 2 \
                 or not all(isinstance(number, int) for number in value)\
                 or not all(number >= 0 for number in value):
-            raise (TypeError("position must be a tuple of 2 positive integers"))
+            raise (TypeError("position must be a tuple\
+             of 2 positive integers"))
 
         self.__position = value
 
