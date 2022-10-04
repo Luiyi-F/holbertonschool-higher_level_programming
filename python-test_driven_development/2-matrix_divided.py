e444613b934b4c7881f5e7f78d46372dbf0ffcd5
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-04 17:54:19 -0500

    [TASK 2, FIXED]: PEP8

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,10 +18,9 @@ def matrix_divided(matrix, div):
     if type(matrix) is not list:
         raise (TypeError("matrix must be a matrix (list of lists) of integers/floats"))
 
-    for idx in matrix:
-        for number in idx:
-            if not (isinstance(number, (int, float))):
-                raise (TypeError("matrix must be a matrix (list of lists) of integers/floats"))
+    if not (isinstance(number, (int, float)) for number in matrix):
+        raise (
+            TypeError("matrix must be a matrix (list of lists) of integers/floats"))
 
     if not all(map(lambda rows: len(matrix[0]) == len(rows), matrix)):
         raise (TypeError("Each row of the matrix must have the same size"))
