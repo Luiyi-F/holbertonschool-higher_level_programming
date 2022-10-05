adc11480152bce2f4f5f32e4b5bf506f98f6c51c
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-04 22:37:39 -0500

    [TASK 4, FEATURES]: prints a text with two lines after each of these chars: ., ?, :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+This is the 5-text_indentation module.
+
+This module supplies one function, text_indention().
+For example:
+
+>>> text_indentation(""Lorem ipsum dolor sit amet, consectetur adipiscing elit."")
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+"""
+
+
+def text_indentation(text):
+    """
+    Given one argument (it`s a text)
+    Return indentation text
+    """
+    if not isinstance(text, str):
+        raise (TypeError("text mus be a string"))
+
+    for indx in range(len(text)):
+        if text[indx] == ' ' and \
+                text[indx - 1] == '.' or \
+                text[indx - 1] == '?' or \
+                text[indx - 1] == ':':
+            continue
+
+        print(text[indx], end="")
+        if text[indx] == '.' or \
+                text[indx] == '?' or \
+                text[indx] == ':':
+            print("\n")
