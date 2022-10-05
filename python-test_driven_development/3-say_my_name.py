697d0fca7121c227bc47cc5d88bb89b7cd8e80a4
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-04 20:27:47 -0500

    [TASK 3, FEATURES]: prints message My name is <first_name> <last_name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+This is the Say may name module.
+
+This module supplies one function, say_my_name().
+For example:
+
+>>> say_my_name("John", "Smith")
+My name is John Smith
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Given two arguments first name and last name 
+    (both string)
+    Return a message with both string
+    """
+    if type(first_name) is not str:
+        raise (TypeError("first_name must be a string"))
+
+    if type(last_name) is not str:
+        raise (TypeError("last_name must be a string"))
+
+    return (print("My name is {} {}".format(first_name, last_name)))
