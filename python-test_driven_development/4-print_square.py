d415904d134e1e7a0d63f912b3bf3be155c843c9
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-04 21:17:57 -0500

    [TASK 4, FEATURES]: prints a squares with the character #

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+This is the print square module.
+
+This module supplies one function, print_square().
+For example:
+
+>>> print_square(4)
+
+####
+####
+####
+####
+"""
+
+
+def print_square(size):
+    """
+    Given one argument, this is a lenght of the square
+    Return prints lenght of the square
+    """
+    if not isinstance(size, int) or type(size) is float\
+            and size < 0:
+        raise (TypeError("size must be an integer"))
+
+    if size < 0:
+        raise (ValueError("size must be >= 0"))
+
+    for height in range(size):
+        for width in range(size):
+            print("#", end="")
+        print()
