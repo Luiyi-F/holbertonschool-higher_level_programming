c3d2f0cd5e780c830ac758b97e8ef0b9f1cc2f8e
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 12:10:42 -0500

    [TASK 2, FEATURES]: return True if the object is instace of the class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module is same class with is_same_class() function:
+return True if the object is isntace of the class,
+otherwise False
+
+Example:
+>>> is_same_class(4, int):
+True
+>>> is_same_class("4", int):
+False
+"""
+
+
+def is_same_class(obj, a_class):
+    """Return True if the object is instance
+    of the class
+    Arguments: Object(obj), object class(a_class)"""
+    if not isinstance(obj, a_class):
+        return (False)
+
+    return (True)
