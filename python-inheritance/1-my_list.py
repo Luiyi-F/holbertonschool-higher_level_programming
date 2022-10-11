0470e5bf66a6bcbf79d81d489d0df20fc3e4694c
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 12:53:38 -0500

    [TASK 1, FEATURES]: print sorted list in MyList class

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module my_list with MyList class and print_sorted() function:
+print a sorted list
+
+Example:
+>>> my_list.print_sorted()
+[1, 2, 3, 4, 5]
+"""
+
+
+class MyList:
+    """MyList class"""
+
+    def print_sorted(self):
+        """Sorted MyList class"""
+        print(sorted(self))
