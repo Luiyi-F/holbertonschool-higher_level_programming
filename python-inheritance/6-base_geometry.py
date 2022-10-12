8fbe9c546d2a7169590864923c43a691ce22c4f8
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 21:49:23 -0500

    [TASK 6, FEATURES]: add public instances method, this method raises a E xeption

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """Initization BaseGeometry"""
+
+    def area(self):
+        """
+        Raises exeption with the message
+        "area() is not implemented"
+        """
+        raise (Exception("area() is not implemented"))
