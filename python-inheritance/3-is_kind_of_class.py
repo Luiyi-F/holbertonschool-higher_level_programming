0e83edb143687d69d7fce0b964f814532ed0121f
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 21:09:28 -0500

    [TASK 3, FEATURES]: this function retunr True id the object is an instance of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module is kind of class with is_kind_of_class() function:
+return True if the object is isntace of the class,
+otherwise False
+
+Example:
+>>> def is_kind_of_class(4, int):
+True
+>>> is_same_class(4, object):
+True
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if the object is instance
+    of the class
+    Arguments: Object(obj), object class(a_class)"""
+    return isinstance(obj, a_class)
