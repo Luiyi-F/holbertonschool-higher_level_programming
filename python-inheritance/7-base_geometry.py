9abda107451b48a2f46b8fe20388d662bc8c9abc
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 22:02:30 -0500

    [TASK 7, FEATURES]: add integer validator if value is not int and if less or equal 0

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module BaseGeometry class
+"""
+
+
+from tokenize import single_quoted
+
+
+class BaseGeometry:
+    """Initization BaseGeometry"""
+
+    def area(self):
+        """
+        Raises exeption with the message
+        "area() is not implemented"
+        """
+        raise (Exception("area() is not implemented"))
+
+    def integer_validator(self, name, value):
+        """
+        Raise a TypeError, if value is not string
+        Raise a ValueError, if value is less or equal 0
+        """
+        if type(value) is not int:
+            raise (TypeError("{} must be an integer".format(name)))
+        if value <= 0:
+            raise (ValueError("{} must be greater than 0".format(name)))
