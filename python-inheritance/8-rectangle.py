2d229fc3d3cf4f09ef0673938bbd0d1d297b9f0a
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 22:29:01 -0500

    [TASK 8, FEATURES]: create Rectangle class with inhertis BaseGeometry class and use ineger_Validator() function with the attribute instance

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module Ractangle class with
+inherits BaseGemetry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Initization Rectangle"""
+
+    def __init__(self, width, height):
+        """
+        width and height is a private instance
+        Validation width and height must be
+        positive integers 
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__width = width
+        self.__height = height
