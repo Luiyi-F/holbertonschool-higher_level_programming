5c4c227f5571e7d21ac69d05c90575be7d1d44c9
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-11 22:59:30 -0500

    [TASK 10, FEATURES]: create a Square class with inheritance Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module Square class with
+inherits Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Initization Rectangle"""
+
+    def __init__(self, size):
+        """
+        Size is a private instance
+        Validation size must be
+        positive integers
+        """
+        Rectangle.integer_validator(self, "size", size)
+        self.__size = size
+        super().__init__(size,size)
+
