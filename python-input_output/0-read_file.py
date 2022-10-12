acf8e596e3c6d2d86c60ee1b5a517903bbc30c00
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-12 15:12:49 -0500

    [TASK 0, FEATURES]: use the open function for read the file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 0-read_file with read_file(filename="")
+In this module read the contend of a file
+"""
+
+
+def read_file(filename=""):
+    """
+    Read a expecific file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())
