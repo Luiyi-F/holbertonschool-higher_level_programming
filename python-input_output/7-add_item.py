6e4c24614dc972446cfed908e9cde5d904e0e82c
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-13 00:21:18 -0500

    [TASK 7, FEATURES]: add and use the last two modules

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 7-add_item
+Use with two lasted modules
+"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+open("add_item.json", "a")
+try:
+    file = load_from_json_file("add_item.json")
+except (ValueError):
+    file = []
+save_to_json_file(file + sys.argv[1:], "add_item.json")
