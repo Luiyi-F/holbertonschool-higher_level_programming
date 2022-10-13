8ff4c335f7c2be810ca04f3653823ec67ca84057
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-13 17:19:26 -0500

    [TASK 10, FEATURES]: create student class

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 9-student with Student class
+Initialization for first_name, last_name and age
+Return dictionary for the object
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return description of the dictionary
+        for JSON searilaztion
+        verification if attrs is a list
+        """
+        if type(attrs) is list and [type(idx) == str for idx in attrs]:
+            return {letter: letter_2 for letter, letter_2
+                    in self.__dict__.items() if letter in attrs}
+
+        return (self.__dict__)
