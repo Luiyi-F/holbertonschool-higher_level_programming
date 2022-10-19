2b4728b27be9a57c7d10dcf4a5b8250552e07dd5
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-18 19:35:54 -0500

    [TASK 10, FEATURES]: create Square class with inheritans from Rectangle class and add getter and setter for size attribute and for last add __str__ with message for see the class name, id, x & y and width

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Create Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialization Square class
+        Args:
+            size
+            x=0
+            y=0
+            id=None
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Print message for see id, x & y and width & height"""
+        cls_name = self.__class__.__name__
+        msg_form = "[{}] ({:d}) {:d}/{:d} - {:d}"
+        return msg_form.format(cls_name, self.id, self.x, self.y, self.width)
