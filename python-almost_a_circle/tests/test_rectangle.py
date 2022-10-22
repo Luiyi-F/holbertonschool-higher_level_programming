7946b95078948786ea3b9e2633c0e69477c2eb3d
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-21 19:14:04 -0500

    [TASK 0, TEST FILE Rectangle]: add correct test case

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+"""
+Rectangle test
+"""
+import unittest
+
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestRectangle(unittest.TestCase):
+    """Initialization Test base class"""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_IntId(self):
+        """Integer id assignation"""
+        r = Rectangle(12, 4)
+        self.assertEqual(r.id, 1)
+
+        r1 = Rectangle(12, 4, 1)
+        self.assertEqual(r1.id, 2)
+
+        r2 = Rectangle(12, 4, 1, 1)
+        self.assertEqual(r2.id, 3)
+
+        r3 = Rectangle(12, 4, 1, 1, 5)
+        self.assertEqual(r3.id, 5)
+
+    def test_NegParameter(self):
+        """Negative integer in parameter assignation"""
+        with self.assertRaises(ValueError):
+            r = Rectangle(-12, 4)
+
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(12, -4)
+
+        with self.assertRaises(ValueError):
+            r2 = Rectangle(12, 4, -1)
+
+        with self.assertRaises(ValueError):
+            r3 = Rectangle(12, 4, 1, -1)
+
+    def test_StrParameter(self):
+        """string in parameter assignation"""
+        with self.assertRaises(TypeError):
+            r = Rectangle('12', 4)
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(12, '4')
+
+        with self.assertRaises(TypeError):
+            r2 = Rectangle(12, 4, '1')
+
+        with self.assertRaises(TypeError):
+            r3 = Rectangle(12, 4, '1')
+
+        with self.assertRaises(TypeError):
+            r4 = Rectangle(12, 4, 1, '1')
+
+    def test_Area(self):
+        """Result area function"""
+        r = Rectangle(12, 4)
+        self.assertEqual(r.area(), 48)
+
+    def test__Str__(self):
+        """Result str function"""
+        r = Rectangle(12, 4, 1, 1, 5)
+        self.assertEqual(str(r), "[Rectangle] (5) 1/1 - 12/4")
+
+    def test_Dictionary(self):
+        r = Rectangle(12, 4, 1, 1, 5)
+        self.assertEqual(r.to_dictionary(), {
+                         'width': 12, 'height': 4, 'x': 1, 'y': 1, 'id': 5})
+
+    def test_Update(self):
+        """Result update"""
+        r = Rectangle(1, 1, 1, 1, 1)
+        r.update(50, 12, 4, 11, 11)
+        self.assertEqual(r.width, 12)
+        self.assertEqual(r.height, 4)
+        self.assertEqual(r.x, 11)
+        self.assertEqual(r.y, 11)
+        self.assertEqual(r.id, 50)
+
+    def test_UpdateKwargs(self):
+        """Result kwargs"""
+        r = Rectangle(1, 1, 1, 1)
+        r.update(x=11, height=4, width=12, y=11)
+        self.assertEqual(r.width, 12)
+        self.assertEqual(r.height, 4)
+        self.assertEqual(r.x, 11)
+        self.assertEqual(r.y, 11)
+        self.assertEqual(r.id, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
