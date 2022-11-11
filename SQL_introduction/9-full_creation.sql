b1fc78a240523acb9d4595d53f3bba0b98cc962a
Luiyi_FHB <luiyifernando5@gmail.com>
2022-11-11 14:29:38 -0500

    [TASK 9]: change double quotes

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- Script create full table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
--- Insert a new row
-INSERT INTO second_table VALUES 
+INSERT INTO second_table (id, name, score) VALUES 
 (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bod", 14),
