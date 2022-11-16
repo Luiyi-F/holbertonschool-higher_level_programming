4cbef7dfdf457da2e104b7516dfd86ddddf7da81
Luiyi_FHB <luiyifernando5@gmail.com>
2022-11-16 15:40:59 -0500

    [TASK 3]: Once again, write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Conect the database"""
+import sys
+import MySQLdb
+
+
+def mysqlconnect():
+    db_connection = None
+    db_connection = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE states.name\
+        '%(username)s' ORDER BY states.id", {"username": sys.argv[4]})
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db_connection.close()
+
+
+if __name__ == '__main__':
+    mysqlconnect()
