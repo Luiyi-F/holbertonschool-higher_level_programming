f6a6caf965b7c38c5f0d782802e55c0f65a43566
Luiyi_FHB <luiyifernando5@gmail.com>
2022-11-16 15:08:15 -0500

    [TASK 1]: lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ def mysqlconnect():
 
     cursor = db_connection.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'"
-                   "COLLATE latin1_General_CS;")
+                   "COLLATE Latin1_General_CS;")
     states = cursor.fetchall()
 
     for state in states:
