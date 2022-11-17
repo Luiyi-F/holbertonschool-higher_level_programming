6229c811ff9e310fe203c517f664a32af7134436
Luiyi_FHB <luiyifernando5@gmail.com>
2022-11-16 20:26:14 -0500

    [TASK 13]: Delete with function

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,13 +14,13 @@ def mysqlconnect():
     {sys.argv[2]}@localhost/{sys.argv[3]}", pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    with Session(engine) as session:
-        for state in session.query(State).\
-                order_by(State.id).\
-                filter(State.name.contains("a")):
-            session.delete(state)
-        session.commit()
-        session.close()
+    session = Session(engine)
+    for state in session.query(State).\
+            order_by(State.id).\
+            filter(State.name.contains("a")):
+        session.delete(state)
+    session.commit()
+    session.close()
 
 
 if __name__ == '__main__':
