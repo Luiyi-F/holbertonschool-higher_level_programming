1a5e60f4ab89275b1288f4e1bb5bb55fa745dea4
Luiyi_FHB <luiyifernando5@gmail.com>
2022-11-16 19:54:05 -0500

    [TASK 8]: add Firs method

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,7 +12,7 @@ def mysqlconnect():
     engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:\
         {sys.argv[2]}@localhost/{sys.argv[3]}")
     with Session(bind=engine) as session:
-        x = session.query(State).all()
+        x = session.query(State).first()
         if x is None:
             print("Nothing")
 
