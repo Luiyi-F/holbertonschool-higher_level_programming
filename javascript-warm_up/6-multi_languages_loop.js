33c004a1afbee84a9c2ba4798d4e79c7c13c28e5
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-19 13:28:05 -0500

    [TASK 6]: user for loop but print the three different name

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// print different language with a loop
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let index = 0; index < array.length; index++) {
+  console.log(array[index])
+}
