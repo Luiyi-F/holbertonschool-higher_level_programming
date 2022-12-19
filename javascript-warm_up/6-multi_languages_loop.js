2ae168022d0fa342cba26d2bca0383b3b5f756ca
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-19 13:29:09 -0500

    [TASK 6]: add semmicolom in line 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let index = 0; index < array.length; index++) {
-  console.log(array[index])
+  console.log(array[index]);
 }
