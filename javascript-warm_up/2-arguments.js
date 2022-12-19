0ad7f6b5bea608428ae2b4c3a23fb6cffdf2f9a6
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-19 12:02:51 -0500

    [TASK 2]: add correct iteraton

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -6,6 +6,10 @@ if (argv.length < 3) {
   console.log('No argument');
 }
 
-if (argv.length >= 3) {
+if (argv.length === 3) {
   console.log('Argument found');
+}
+
+if (argv.length >= 3) {
+  console.log('Arguments found');
 }
