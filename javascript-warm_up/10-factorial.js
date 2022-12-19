931eba3b7a3c05f2ff4785f5f0ae165fef596258
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-19 15:38:39 -0500

    [TASK 10]: add correct style

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -5,9 +5,9 @@ const num = parseInt(argv[2]);
 
 console.log(factorial(num));
 
-function factorial(n) {
-  if (!n)
+function factorial (n) {
+  if (!n) {
     return 1;
-
+  }
   return n * factorial(n - 1);
 }
