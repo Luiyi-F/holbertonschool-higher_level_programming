e3d82d5558637bd9be8b7860793d99619a8835c0
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-20 16:11:27 -0500

    [TASK 6]: create a new function for change the char

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// create function for print the size with diferent char
+const Rectangle = require('./4-rectangle');
+
+module.exports = class square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    }
+
+    if (c) {
+      for (let index = 0; index < this.height; index++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
