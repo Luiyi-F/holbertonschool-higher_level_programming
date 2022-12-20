e79773ae3fee92d4bf0aa6005a8c1851d30b3c49
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-20 16:49:23 -0500

    [TASK 7]: use for of in this functions for create a numbered occuremces

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// write a count ocurrence in a list
+exports.nbOccurences = function (list, searchElement) {
+  let counter = +1;
+  for (const index of list) {
+    if (searchElement === index) {
+      counter++;
+    }
+  }
+  return counter;
+};
