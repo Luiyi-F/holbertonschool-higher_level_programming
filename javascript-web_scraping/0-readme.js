c7f2cf207dac30652fd840c3771fcc4d70377252
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-21 15:53:00 -0500

    [TASK 0]: use the correctly function fileRead

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, (error, data)) => {
-  if (!error) {
-    console.log(data.toString());
-  }
-  console.log(error);
-}
+fs.readFile(file, (error, data) => {
+  if (error) console.log(error);
+  if (!error) console.log(data.toString());
+});
