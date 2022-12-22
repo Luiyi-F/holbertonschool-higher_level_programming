37ad4582d79abbc9781e8cce0e512f340364d113
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-22 15:07:33 -0500

    [TASK 6]: cumputes the task complete

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id
+
+const url = process.argv[2];
+const request = require('request');
+
+request.get(url, (error, response, data) => {
+  const dataURL = JSON.parse(data);
+  const completTASK = {};
+
+  if (!error && response.statusCode === 200) {
+    dataURL.forEach(task => {
+      if (task.completed === true) {
+        if (!(task.userId in completTASK)) completTASK[task.userId] = 1;
+        if (task.userId in completTASK) completTASK[task.userId]++;
+      }
+    });
+    console.log(completTASK);
+  }
+});
