2f7fbbc2e5b98a075f2436de14c82f54779c3b96
Luiyi_FHB <luiyifernando5@gmail.com>
2022-12-23 16:31:48 -0500

    [TASK 4]: get wedge

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (error, response, data) {
+  if (!error) {
+    const result = JSON.parse(data).results;
+    console.log(result.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0))
+  }
+});
